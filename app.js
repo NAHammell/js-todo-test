db01271ab54d53db9902ebf632a7dbafe810925b
Nick Hammell <nicholas.a.hammell@gmail.com>
2023-11-14 14:22:00 -0500

    Add task creating functionality

---
+++ app.js
@@ -0,0 +1,30 @@
+const addTask = document.querySelector('#add-task');
+const taskContainer = document.querySelector('#task-container');
+const inputTask = document.querySelector('#input-task');
+
+addTask.addEventListener('click', function() {
+    if (inputTask.value === '') {
+        alert('Please enter a task.');
+    } else {
+        let task = document.createElement('div');
+        task.classList.add('task');
+    
+        let li = document.createElement('li');
+        li.innerText = `${inputTask.value}`;
+        task.appendChild(li);
+    
+        let checkButton = document.createElement('button');
+        checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+        checkButton.classList.add('check-task');
+        task.appendChild(checkButton);
+    
+        let deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+        deleteButton.classList.add('delete-task');
+        task.appendChild(deleteButton);
+
+        taskContainer.appendChild(task);
+    }
+
+    inputTask.value = '';
+});
