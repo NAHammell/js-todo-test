b56bf71285e53664b30f0b13f6c4f35458257d3a
Nick Hammell <nicholas.a.hammell@gmail.com>
2023-11-14 14:32:51 -0500

    Add strikethrough and delete functions

--- app.js
+++ app.js
@@ -17,11 +17,25 @@ addTask.addEventListener('click', function() {
         checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
         checkButton.classList.add('check-task');
         task.appendChild(checkButton);
+
+        checkButton.addEventListener('click', function() {
+            let parentText = checkButton.parentElement;
+            
+            if (parentText.style.textDecoration != 'line-through') {
+                checkButton.parentElement.style.textDecoration = 'line-through';
+            } else {
+                checkButton.parentElement.style.textDecoration = 'none';
+            }
+        });
     
         let deleteButton = document.createElement('button');
         deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
         deleteButton.classList.add('delete-task');
         task.appendChild(deleteButton);
+
+        deleteButton.addEventListener('click', function() {
+            deleteButton.parentElement.remove();
+        });
 
         taskContainer.appendChild(task);
     }
